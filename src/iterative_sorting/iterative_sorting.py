a67a70e91899e98778f726eae27e3dc031fdfe97
trucane <trufashionista2013@gmail.com>
2019-12-09 19:17:03 -0600

    fixed recursive code:

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -24,12 +24,18 @@ print(selection_sort(arraysss))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+
+    #variable = length of array 
     x = len(arr)
 
     for check_num in range(0, x):
+        #range is 0 (start) to value of x 
         for i in range(0, x - check_num - 1):
+            # for each element in provided array from start(0) to 7-position-1
             if arr[i] > arr[i + 1]:
+                #if 1st iteration in array is less than 2nd iteration of array:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                #then swap posotions greater goes to the right then right position goes to the left 
 
 
     return arr
